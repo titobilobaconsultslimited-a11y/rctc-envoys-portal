0141801fa8874f5459fc8e2db5102687f448c84c
titobilobaconsultslimited-a11y <titobilobaconsultslimited@gmail.com>
2026-04-17 20:23:33 +0100

    Add YP20 logo to login page and all portal headers

--- js/app.js
+++ js/app.js
@@ -136,6 +136,7 @@ function renderHeader(studentMatric) {
         <div class="header-logos">
           <img src="Media/rcbc logo.png" alt="RCTC Logo" class="header-logo">
           <img src="Media/envoys logo.png" alt="Envoys Logo" class="header-logo">
+          <img src="Media/YP20.jpg" alt="YP20 Logo" class="header-logo">
         </div>
         <div class="header-text">
           <h1>Redeemed Christian Theology College</h1>
@@ -238,6 +239,7 @@ function renderSodHeader(studentMatric) {
         <div class="header-logos">
           <img src="Media/rcbc logo.png" alt="RCTC Logo" class="header-logo">
           <img src="Media/envoys logo.png" alt="Envoys Logo" class="header-logo">
+          <img src="Media/YP20.jpg" alt="YP20 Logo" class="header-logo">
         </div>
         <div class="header-text">
           <h1>Redeemed Christian Theology College</h1>
